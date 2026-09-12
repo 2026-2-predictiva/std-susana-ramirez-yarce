a6308503faf57d8e249c1d4718e1d4252750e18c
sramirezy <sramirezy@unal.edu.co>
2026-09-12 10:43:05 -0500

    Taller 1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Ejecute los siguientes comandos en el terminal:
 python3 -m venv .venv
 source .venv/bin/activate
 source setup.sh
-```
+``` 
 
 # Configuración en Windows
 
--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 @echo off
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+py -m pip install --upgrade pip
+py -m pip install -r requirements.txt
